30c7dca09eb5d2e6af872136ba75670e20c84599
Nehal Patel <nehal@alum.mit.edu>
2025-10-11 06:37:14 -0400

    sync

--- src/fire_prox/test_fp.py
+++ src/fire_prox/test_fp.py
@@ -1,2 +1,25 @@
+import os
+
+from google.cloud import firestore
+
 def test_fire_prox():
-    assert True
+
+    os.environ["GRPC_VERBOSITY"] = "NONE"
+    os.environ["FIRESTORE_EMULATOR_HOST"] = "localhost:8080"
+
+
+    # Initialize Firestore client (uses emulator if env var is set)
+    db = firestore.Client(project="fire-prox-testing")
+
+    # Add a document to the 'users' collection
+    doc_ref = db.collection("users").document()
+    doc_ref.set({"name": "Test User", "email": "testuser@example.com", "created": firestore.SERVER_TIMESTAMP})
+
+    print(f"Added document with ID: {doc_ref.id}")
+
+    ## query the database
+    query = db.collection("users")
+    results = query.stream()
+
+    for doc in results:
+        print(f"Document ID: {doc.id}, Data: {doc.to_dict()}")
